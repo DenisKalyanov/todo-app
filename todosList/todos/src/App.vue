2451ad68ff9756d5743ec975d6305028f5faf671
i-barysiuk <i.barysiuk@godeltech.com>
2021-01-05 11:28:30 +0300

    feature/P1-3: -refactoring to Vue layout

---
+++ todosList/todos/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <TodosList />
+  </div>
+</template>
+
+<script>
+import TodosList from "./components/TodosList.vue";
+
+export default {
+  name: "App",
+  components: {
+    TodosList,
+  },
+};
+</script>
+
+<style>
+</style>
