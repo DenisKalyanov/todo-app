ab87cd1c73f5be0cac0bc5fc25492362947c5f68
Denis Kalyanov <denzarj@mail.ru>
2021-02-10 14:36:21 +0300

    feature/P1-3: -was beed created unit tests for mutation store, and fixed getters of store

--- src/store/modules/taskMethods.js
+++ src/store/modules/taskMethods.js
@@ -1,69 +1,59 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-export default{
-    strict: true,
-    actions:{
-        setState(context) {
-            context.commit('setState');
-        },
+export default {
+  actions: {
+    setState(context) {
+      context.commit("setState");
     },
-    mutations:{
-        setState(state){
-            state.tasks = JSON.parse(localStorage.getItem('tasks'));
-        },
-        createTask(state, newTask){
-            if(newTask){
-                const task = {message: newTask, checked: false, id: uuidv4};
-                state.tasks.push(task);
-                localStorage.setItem('tasks', JSON.stringify(state.tasks));
-            }
-
-        },
-        changeStatus(state, targetId){
-            state.tasks = state.tasks.map(task => {
-                return task.id === targetId ? { ...task, checked: !task.checked }: task
-            });
-            localStorage.setItem('tasks', JSON.stringify(state.tasks)); 
-        },
-      
-        changeTypeTasks(state, targetName){
-
-            if(targetName==="deleteCompleted"){
-                state.tasks = state.tasks.filter(task => !task.checked );          
-                localStorage.setItem('tasks', JSON.stringify(state.tasks));
-                return state.statusButton = "allTasks";
-
-            } 
-            state.statusButton = targetName;
-        }
+  },
+  mutations: {
+    setState(state) {
+      state.tasks = JSON.parse(localStorage.getItem("tasks")) || new Array();
     },
-    state:{
-        tasks: [],
-        statusButton: "allTasks",
+    createTask(state, newTask) {
+      if (newTask) {
+        const task = { message: newTask, checked: false, id: uuidv4() };
+        console.log(task);
+        console.log(state);
+        state.tasks.push(task);
+        localStorage.setItem("tasks", JSON.stringify(state.tasks));
+      }
     },
-    getters:{
+    changeStatus(state, targetId) {
+      state.tasks = state.tasks.map((task) => {
+        return task.id === targetId
+          ? { ...task, checked: !task.checked }
+          : task;
+      });
+      localStorage.setItem("tasks", JSON.stringify(state.tasks));
+    },
 
-        allTasks(state){
-            return state.tasks;
-        },
-
-        activeTasks(state){
-            return state.tasks.filter(task=> !task.checked);
-
-        },
-        completedTasks(state){
-            return state.tasks.filter(task=>task.checked);
-
-        },
-        chosenTasks(state, getters ){
-            
-            if(state.statusButton==="allTasks"){
-                return getters.allTasks
-            }else if (state.statusButton==="activeTasks"){
-                return getters.activeTasks
-            }else  if (state.statusButton==="completedTasks"){
-                return getters.completedTasks
-            }
-    }
-},
-}
+    changeTypeTasks(state, targetName) {
+      if (targetName === "deleteCompleted") {
+        state.tasks = state.tasks.filter((task) => !task.checked);
+        localStorage.setItem("tasks", JSON.stringify(state.tasks));
+        return (state.statusButton = "allTasks");
+      }
+      state.statusButton = targetName;
+    },
+  },
+  state: {
+    tasks: [],
+    statusButton: "allTasks",
+  },
+  getters: {
+    allTasks(state) {
+      return state.tasks;
+    },
+    chosenTasks(state, getters) {
+      switch (state.statusButton) {
+        case "allTasks":
+          return getters.allTasks;
+        case "activeTasks":
+          return state.tasks.filter((task) => !task.checked);
+        case "completedTasks":
+          return state.tasks.filter((task) => task.checked);
+      }
+    },
+  },
+};
--- tests/unit/store/store.spec.js
+++ tests/unit/store/store.spec.js
@@ -1,17 +1,70 @@
-import { createLocalVue } from '@vue/test-utils'
-import Vuex from 'vuex'
-import storeConfig from './store.cfg'
-import { cloneDeep } from 'lodash'
+import { createLocalVue } from "@vue/test-utils";
+import Vuex from "vuex";
+import storeConfig from "./store.cfg";
+import { cloneDeep } from "lodash";
 
-test('adding a new object to state', () => {
-    const localVue = createLocalVue()
-    localVue.use(Vuex)
-    // 
-    const store = new Vuex.Store(cloneDeep(storeConfig))
-    let newTask = "newTAsk";
+describe("unit tests mutation state", () => {
+  test("adding a new object to state ", () => {
+    const localVue = createLocalVue();
+    localVue.use(Vuex);
+
+    const store = new Vuex.Store(cloneDeep(storeConfig));
+    let newTask = "newTodo";
     expect(store.state.tasks.length).toBe(0);
 
-    store.commit('createTask', newTask)
-    expect(store.state.tasks[0]).toBe({ message: newTask, checked: false, id: 1})
-    // https://vue-test-utils.vuejs.org/ru/guides/#%D1%82%D0%B5%D1%81%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5-vuex-%D0%B2-%D0%BA%D0%BE%D0%BC%D0%BF%D0%BE%D0%BD%D0%B5%D0%BD%D1%82%D0%B0%D1%85
-}) 
+    store.commit("createTask", newTask);
+    expect(store.state.tasks[0].message).toBe("newTodo");
+    expect(store.state.tasks[0].checked).toBe(false);
+    expect(store.state.tasks[0].id).toBeTruthy();
+  });
+
+  test("change ckecked flag", () => {
+    const localVue = createLocalVue();
+    localVue.use(Vuex);
+    const store = new Vuex.Store(cloneDeep(storeConfig));
+
+    let newTask = "newTask";
+    store.commit("createTask", newTask);
+
+    let id = store.state.tasks[0].id;
+    let checked = store.state.tasks[0].checked;
+
+    store.commit("changeStatus", id);
+    expect(store.state.tasks[0].checked).toBe(!checked);
+  });
+
+  test("clear completed tasks", () => {
+    const localVue = createLocalVue();
+    localVue.use(Vuex);
+    const store = new Vuex.Store(cloneDeep(storeConfig));
+
+    let newTask = "newTask";
+    store.commit("createTask", newTask);
+
+    store.state.tasks[0].checked = true;
+
+    const nameButtonForDelete = "deleteCompleted";
+
+    store.commit("changeTypeTasks", nameButtonForDelete);
+    expect(store.state.tasks.length).toBe(0);
+    expect(store.state.statusButton).toBe("allTasks");
+  });
+
+  test("check how working button for change statusButto name", () => {
+    const localVue = createLocalVue();
+    localVue.use(Vuex);
+    const store = new Vuex.Store(cloneDeep(storeConfig));
+
+    let newTask = "newTask";
+    store.commit("createTask", newTask);
+
+    const nameButtonCompleted = "completedTasks";
+    const nameButtonActive = "activeTasks";
+
+    store.commit("changeTypeTasks", nameButtonCompleted);
+    expect(store.state.statusButton).toBe("completedTasks");
+
+    store.commit("changeTypeTasks", nameButtonActive);
+    expect(store.state.statusButton).toBe("activeTasks");
+  });
+});
